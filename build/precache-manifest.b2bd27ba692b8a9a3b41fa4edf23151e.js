35479dbb6bd48e6012492a8b0591f6457e5b5a39
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-07-16 22:33:10 +0500

    Fixed Validation

---
+++ build/precache-manifest.b2bd27ba692b8a9a3b41fa4edf23151e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c9904a98b041d4f2151cbee4314fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84264e17eb187db60b9d",
+    "url": "/static/css/main.07e8422a.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84264e17eb187db60b9d",
+    "url": "/static/js/main.04e40e5b.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
